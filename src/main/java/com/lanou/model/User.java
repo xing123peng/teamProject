efe9854f8073e941633524e83715b6bb9f4050d0
彭卫星 <peng@pengweixing.local>
2018-07-26 09:48:09 +0800

    project build

---
+++ src/main/java/com/lanou/model/User.java
@@ -0,0 +1,63 @@
+package com.lanou.model;
+
+public class User {
+    private Integer userId;
+
+    private String userName;
+
+    private String userPassword;
+
+    private Integer deleteid;
+
+    private Integer teacherId;
+
+    private String userNames;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword == null ? null : userPassword.trim();
+    }
+
+    public Integer getDeleteid() {
+        return deleteid;
+    }
+
+    public void setDeleteid(Integer deleteid) {
+        this.deleteid = deleteid;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getUserNames() {
+        return userNames;
+    }
+
+    public void setUserNames(String userNames) {
+        this.userNames = userNames == null ? null : userNames.trim();
+    }
+}
